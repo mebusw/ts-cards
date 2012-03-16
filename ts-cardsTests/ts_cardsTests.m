5f4b1cd9593b8ef77a86705aa1af20d3b3144808
mebusw <mebusw@163.com>
2012-03-16 12:20:13 +0800

    add lib OCMock

--- ts-cardsTests/ts_cardsTests.m
+++ ts-cardsTests/ts_cardsTests.m
@@ -7,6 +7,9 @@
 //
 
 #import "ts_cardsTests.h"
+#import "OCMock/OCMock.h"
+#import "MasterViewController.h"
+
 
 @implementation ts_cardsTests
 
@@ -29,4 +32,23 @@
     //STFail(@"Unit tests are not implemented yet in ts-cardsTests");
 }
 
+-(void) testOCMockExample {
+    MasterViewController *controller = [[MasterViewController alloc] init];
+    NSIndexPath *dummyIndexPath = [NSIndexPath indexPathForRow:3 inSection:1];
+    id tableViewMock = [OCMockObject mockForClass:[UITableView class]];
+    [[tableViewMock expect] deleteRowsAtIndexPaths:[NSArray arrayWithObject:dummyIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+    
+    // Invoke functionality to be tested
+    // If you want to see the test fail you can, for example, change the editing style to UITableViewCellEditingStyleNone. In
+    // that case the method in the controller does not make a call to the table view and the mock will raise an exception when
+    // verify is called further down.
+    
+    [controller tableView:tableViewMock commitEditingStyle:UITableViewCellEditingStyleDelete forRowAtIndexPath:dummyIndexPath];
+    
+    // Verify that expectations were met
+    
+    [tableViewMock verify];
+
+    
+}
 @end
