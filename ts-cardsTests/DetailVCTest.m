663ad9fea6aeeadaad8f61d173bb48940741d067
mebusw <mebusw@163.com>
2012-03-16 17:10:36 +0800

    add test class for Detail VC

--- ts-cardsTests/DetailVCTest.m
+++ ts-cardsTests/DetailVCTest.m
@@ -7,7 +7,35 @@
 //
 
 #import "DetailVCTest.h"
+#import "DetailViewController.h"
+#import "OCMock/OCMock.h"
+#import "TSCard.h"
+
 
 @implementation DetailVCTest
 
+- (void)setUp {
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)test_configureView {
+    DetailViewController *controller = [[DetailViewController alloc] init];
+    
+//    NSIndexPath *dummyIndexPath = [NSIndexPath indexPathForRow:3 inSection:1];
+//    id tableViewMock = [OCMockObject mockForClass:[UITableView class]];
+//    [[tableViewMock expect] dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    [controller viewDidLoad];
+    
+//    [tableViewMock verify];
+    STAssertNil(controller.detailItem, @"");
+}
 @end
